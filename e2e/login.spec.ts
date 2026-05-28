597fddf036d58428a40a69e845591e7caeb05d70
isabellyccamargo <camargoisa114@gmail.com>
2026-05-28 09:18:11 -0300

    test: implementando ambiente apra teste e2e

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,6 +1,6 @@
-import { test, expect } from "@playwright/test";
+// import { test, expect } from "@playwright/test";
 
-test("login page should have title", async ({ page }) => {
-  await page.goto("http://localhost:5173/login");
-  await expect(page).toHaveTitle(/login/i);
-});
+// test("login page should have title", async ({ page }) => {
+//   await page.goto("http://localhost:5173/login");
+//   await expect(page).toHaveTitle(/login/i);
+// });
